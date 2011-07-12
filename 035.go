e79f777475f1900f5656a6b63c77d00cd9d4c2ba
Mustafa Zengin <mzengin88@gmail.com>
2011-07-13 00:04:49 +0300

    performance: from 58s to 0.1s

--- 035.go
+++ 035.go
@@ -4,65 +4,42 @@ import (
 	"strconv"
 )
 
-var arr []int
-
 func main() {
-	arr = make([]int, 1000000)
-	arr[0] = 2
-	primes := 1
-
-	var prime bool
-	for i := 3; i < 1000000; i++ {
-		prime = true
-	inner:
-		for j := 0; j < primes; j++ {
-			if i%arr[j] == 0 {
-				prime = false
-				break inner
-			}
-		}
-		if prime {
-			arr[primes] = i
-			primes++
-		}
-	}
-	arr = arr[:primes]
-	println(circularPrimes())
-}
-
-func circularPrimes() int {
+	arr := make([]bool, 1000000)
+	arr[1] = true
+	prime := 3
+	count := 13
+	var k, tmp, localCount int
 	var str string
-	var local int
-	var found bool
-	count := 0
-	for i := 0; i < len(arr); i++ {
-		found = true
-		str = strconv.Itoa(arr[i])
-	inner:
-		for j := 0; j < len(str)-1; j++ {
-			str = str[1:] + str[0:1]
-			if local, _ = strconv.Atoi(str); !search(local, 0, len(arr)-1) {
-				found = false
-				break inner
+primeloop:
+	for {
+		for k = prime * 2; k < len(arr); k += prime {
+			arr[k] = true
+		}
+		for k = prime + 2; k < len(arr) && arr[k]; k += 2 {
+		}
+		if k < len(arr) {
+			prime = k
+			str = strconv.Itoa(prime)
+			if prime > 100 {
+				localCount = 1
+				for i := 0; i < len(str)-1; i++ {
+					str = str[1:] + str[0:1]
+					tmp, _ = strconv.Atoi(str)
+					if tmp > prime {
+						continue primeloop
+					} else if !arr[tmp] && tmp%2 != 0 {
+						localCount++
+					} else {
+						continue primeloop
+					}
+				}
+				count += localCount
 			}
+		} else {
+			break
 		}
-		if found {
-			count++
-		}
 	}
-	return count
-}
-
-func search(value, low, high int) bool {
-	if high < low {
-		return false
-	}
-	mid := low + (high-low)/2
-	if arr[mid] > value {
-		return search(value, low, mid-1)
-	} else if arr[mid] < value {
-		return search(value, mid+1, high)
-	}
-	return true
+	println(count)
 }
 
